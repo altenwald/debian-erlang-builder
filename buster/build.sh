78a87224d4725e94502a38de3838a8cbb2328308
Manuel Rubio <manuel@altenwald.com>
2020-10-28 16:00:06 +0000

    remove support for jessie and update creation for stretch and buster

--- buster/build.sh
+++ buster/build.sh
@@ -4,9 +4,11 @@ set -e
 set -x
 
 ERLANG_VSN="${ERLANG_VSN:-19.3}"
+VSN="${VSN:-1.0.0}"
 
 docker build -t erlang_buster .
 docker run -e ERLANG_VSN=$ERLANG_VSN \
+	   -e ERL_TOP=/usr/local/src/otp-$ERLANG_VSN-$VSN \
            -v `pwd`/output:/output \
            -v `pwd`/../input:/input \
            --rm \
